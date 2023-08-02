59a2799eef2148f7ade69d6447a715d00229357f
Erudite885 <erudite885@gmail.com>
2023-08-02 15:23:53 +0100

    assets initiated and packages installed

---
+++ src/assets/index.js
@@ -0,0 +1,63 @@
+import ReactIcon from "./react.svg";
+import App from "./app.svg";
+import Backup from "./backup.svg";
+import Chart from "chart.svg";
+import Calender from "./calender.svg";
+import Conversion from "./conversionicon.svg";
+import Delete from "./delete.svg";
+import Element from "./element.svg";
+import Expand from "./expand.svg";
+import Favicon from "./favicon.svg";
+import Form from "./form.svg";
+import Home from "./home.svg";
+import imgIcon from "./img.svg";
+import Log from "./log.svg";
+import logoIcon from "./logo.svg";
+import NoAvatar from "./noavatar.png";
+import noteIcon from "./note.svg";
+import Notifications from "./notifications.svg";
+import Order from "./order.svg";
+import Post from "./post.svg";
+import Product from "./product.svg";
+import ProductIcon from "./productIcon.svg";
+import Profile from "./profile.svg";
+import RevenueIcon from "./revenueIcon.svg";
+import Search from "./search.svg";
+import Setting from "./setting.svg";
+import Settings from "./settings.svg";
+import User from "./user.svg";
+import UserIcon from "./userIcon.svg";
+import View from "./view.svg";
+
+export {
+  ReactIcon,
+  App,
+  Backup,
+  Chart,
+  Calender,
+  Conversion,
+  Delete,
+  Element,
+  Expand,
+  Favicon,
+  Form,
+  Home,
+  imgIcon,
+  Log,
+  logoIcon,
+  NoAvatar,
+  noteIcon,
+  Notifications,
+  Order,
+  Post,
+  Product,
+  ProductIcon,
+  Profile,
+  RevenueIcon,
+  Search,
+  Setting,
+  Settings,
+  User,
+  UserIcon,
+  View,
+};
