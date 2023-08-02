90407063c53a709bf3620ca17afa85b147080c98
Erudite885 <erudite885@gmail.com>
2023-08-03 00:17:38 +0100

    index page for components and pages section

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+export { default as Navbar } from "./navbar/Navbar";
+export { default as Footer } from "./footer/Footer";
+export { default as Menu } from "./menu/Menu";
+export { default as Single } from "./single/Single";
+export { default as Add } from "./add/Add";
+export { default as BarChartBox } from "./barChartBox/BarChartBox";
+export { default as BigChartBox } from "./bigChartBox/BigChartBox";
+export { default as ChartBox } from "./chartBox/ChartBox";
+export { default as DataTable } from "./dataTable/DataTable";
+export { default as pieChartBox } from "./pieChartBox/PieChartBox";
+export { default as TopBox } from "./topBox/TopBox";
