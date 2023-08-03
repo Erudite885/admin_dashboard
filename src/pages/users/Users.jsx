dd5cc00e3b257ebea517998e5b255ed4b24a3785
Erudite885 <erudite885@gmail.com>
2023-08-03 16:21:17 +0100

    users component and users styling

--- src/pages/users/Users.jsx
+++ src/pages/users/Users.jsx
@@ -1,7 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+// import { GridColDef } from "@mui/x-data-grid";
+
+import { Add, DataTable } from "../../components";
+import { userRows } from "../../data";
 import "./Users.scss";
+
+const columns = [
+  { field: "id", headerName: "ID", width: 90 },
+  {
+    field: "img",
+    headerName: "Avatar",
+    width: 100,
+    renderCell: (params) => {
+      return <img src={params.row.img || "/noavatar.png"} alt="" />;
+    },
+  },
+  {
+    field: "firstName",
+    headerName: "First name",
+    width: 150,
+  },
+  {
+    field: "lastName",
+    headerName: "Last name",
+    width: 150,
+  },
+  {
+    field: "email",
+    headerName: "Email",
+    width: 200,
+  },
+  {
+    field: "phone",
+    headerName: "Phone",
+    width: 200,
+  },
+  {
+    field: "createdAt",
+    headerName: "Created At",
+    width: 200,
+  },
+  {
+    field: "verified",
+    headerName: "Verified",
+    width: 150,
+    type: "boolean",
+  },
+];
+
 const Users = () => {
-  return <div>Users</div>;
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="users">
+      <div className="info">
+        <h1>Users</h1>
+        <button onClick={() => setOpen(true)}>Add New User</button>
+      </div>
+      <DataTable slug="users" columns={columns} rows={userRows} />
+      {open && <Add slug="user" columns={columns} setOpen={setOpen} />}
+    </div>
+  );
 };
 
 export default Users;
