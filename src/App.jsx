a1fd6f3e564172fb51f95f15579b553309503a37
Erudite885 <erudite885@gmail.com>
2023-08-03 01:05:33 +0100

    router, r-query, pages, components & styling to app function

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,67 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { Home, Users, Products, User, Product, Login } from "./pages/index";
+import { Navbar, Menu, Footer } from "./components/index";
+import "./styles/global.scss";
+
+const queryClient = new QueryClient();
 
 const App = () => {
-  return <div>
-    
-  </div>;
+  
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentCOntainer">
+            <QueryClientProvider client={queryClient}>
+              <Outlet />
+            </QueryClientProvider>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/users",
+          element: <Users />,
+        },
+        {
+          path: "/products",
+          element: <Products />,
+        },
+        {
+          path: "/users/:id",
+          element: <User />,
+        },
+        {
+          path: "/products/:id",
+          element: <Product />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+  ]);
+  return <RouterProvider router={router} />;
 };
 
 export default App;
