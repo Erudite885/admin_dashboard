9ab141a2a68b2d2eea942f22a35dd7ef74060e4e
Erudite885 <erudite885@gmail.com>
2023-08-03 12:44:33 +0100

    topdeal img adjusted

--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-import  {
+import {
   Backup,
   CalendarIcon,
   ChartIcon,
@@ -7,6 +7,7 @@ import  {
   Form,
   Home,
   Log,
+  NoAvatar,
   Order,
   Post,
   Product,
@@ -18,7 +19,7 @@ import  {
   noteIcon,
 } from "./assets/index";
 
-export  const menu = [
+export const menu = [
   {
     id: 1,
     title: "main",
@@ -138,49 +139,49 @@ export  const menu = [
 export const topDealUsers = [
   {
     id: 1,
-    img: "https://images.pexels.com/photos/8405873/pexels-photo-8405873.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    img: NoAvatar,
     username: "Elva McDonald",
     email: "elva@gmail.com",
     amount: "3.668",
   },
   {
     id: 2,
-    img: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    img: NoAvatar,
     username: "Linnie Nelson",
     email: "linnie@gmail.com",
     amount: "3.256",
   },
   {
     id: 3,
-    img: "https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    img: NoAvatar,
     username: "Brent Reeves",
     email: "brent@gmail.com",
     amount: "2.998",
   },
   {
     id: 4,
-    img: "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    img: NoAvatar,
     username: "Adeline Watson",
     email: "adeline@gmail.com",
     amount: "2.512",
   },
   {
     id: 5,
-    img: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    img: NoAvatar,
     username: "Juan Harrington",
     email: "juan@gmail.com",
     amount: "2.134",
   },
   {
     id: 6,
-    img: "https://images.pexels.com/photos/1681010/pexels-photo-1681010.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    img: NoAvatar,
     username: "Augusta McGee",
     email: "augusta@gmail.com",
     amount: "1.932",
   },
   {
     id: 7,
-    img: "https://images.pexels.com/photos/874158/pexels-photo-874158.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    img: NoAvatar,
     username: "Angel Thomas",
     email: "angel@gmail.com",
     amount: "1.560",
