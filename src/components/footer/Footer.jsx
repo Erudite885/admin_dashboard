81fd1d9c8b36a687fd69e95879d95f3131e398e0
Erudite885 <erudite885@gmail.com>
2023-08-03 01:36:04 +0100

    footer component and footer styling

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 import "./Footer.scss";
+
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="footer">
+      <span>Admin</span>
+      <span>© Admin Dashboard</span>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
