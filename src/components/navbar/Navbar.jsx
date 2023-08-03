eb74f35c92403750976a3745b70248396ee7ae9b
Erudite885 <erudite885@gmail.com>
2023-08-03 01:21:57 +0100

    navbar component and navbar styling

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import { App, Expand, NoAvatar, Search, Settings, logoIcon } from "../../assets/index";
 import "./Navbar.scss";
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="navbar">
+      <div className="logo">
+        <img src={logoIcon} alt="logo icon" />
+        <span>Admin</span>
+      </div>
+      <div className="icons">
+        <img src={Search} alt="search icon" />
+        <img src={App} alt="app icon" />
+        <img src={Expand} alt="expand icon" />
+        <div className="notification">
+          <img src={Notifications} alt="notification icon" />
+          <span>1</span>
+        </div>
+        <div className="user">
+          <img src={NoAvatar} alt="user icon" />
+          <span>Jane</span>
+        </div>
+        <img src={Settings} alt="settings icon" />
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
