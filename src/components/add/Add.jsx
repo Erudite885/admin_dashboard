c2f5780adc5444c735a70c9c3a4e3116a58baaf4
Erudite885 <erudite885@gmail.com>
2023-08-04 05:41:34 +0100

    add component and add styling

--- src/components/add/Add.jsx
+++ src/components/add/Add.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import "./Add.scss"
+import React from "react";
+import "./Add.scss";
 
-const Add = () => {
+const Add = (props) => {
+  const submitHandler = (e) => {
+    e.preventDefault();
+    props.setOpen(false);
+  };
+
   return (
-    <div className='add'>Add</div>
-  )
-}
+    <div className="add">
+      <div className="modal">
+        <span className="close" onClick={() => props.setOpen(false)}>
+          X
+        </span>
+        <h1>Add New {props.slug}</h1>
+        <form onSubmit={submitHandler}>
+          {props.columns
+            .filter((item) => item.field !== "id" && item.field !== "img")
+            .map((column) => (
+              <div className="item">
+                <label>{column.headerName}</label>
+                <input type={column.type} placeholder={column.field} />
+              </div>
+            ))}
+          <button>Send</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default Add
+export default Add;
