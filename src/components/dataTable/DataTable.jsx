e117453cbd5bc2c2c513781a805c04609f093fed
Erudite885 <erudite885@gmail.com>
2023-08-04 05:27:04 +0100

    datatable component and datatable styling

--- src/components/dataTable/DataTable.jsx
+++ src/components/dataTable/DataTable.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
-import "./DataTable.scss";
-const DataTable = () => {
-  return (
-    <div>DataTable</div>
-  )
-}
+import React from "react";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { Link } from "react-router-dom";
 
-export default DataTable
+import { Delete, View } from "../../assets";
+import "./DataTable.scss";
+
+const DataTable = (props) => {
+  const deleteHandler = (id) => {};
+
+  const actionColumn = {
+    field: "action",
+    headerName: "Action",
+    width: 200,
+    renderCell: (params) => {
+      return (
+        <div className="action">
+          <Link to={`/${props.slug}/${params.row.id}`}>
+            <img src={View} alt="view icon" />
+          </Link>
+          <div className="delete" onClick={() => deleteHandler(params.row.id)}>
+            <img src={Delete} alt="delete icon" />
+          </div>
+        </div>
+      );
+    },
+  };
+
+  return (
+    <div className="dataTable">
+      <DataGrid
+        className="dataGrid"
+        rows={props.rows}
+        columns={[...props.columns, actionColumn]}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 10,
+            },
+          },
+        }}
+        slots={{ toolbar: GridToolbar }}
+        slotProps={{
+          toolbar: {
+            showQuickFilter: true,
+            quickFilterProps: { debounceMs: 500 },
+          },
+        }}
+        pageSizeOptions={[10]}
+        checkboxSelection
+        disableRowSelectionOnClick
+        disableColumnFilter
+        disableDensitySelector
+        disableColumnSelector
+      />
+    </div>
+  );
+};
+
+export default DataTable;
